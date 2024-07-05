929199731e172c75bcaa672f294c206cf761adea
Artyom D <artyom.dorohovich@gmail.com>
2024-07-05 23:12:33 +0400

    first commit

---
+++ src/main/java/com/codehoba/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.codehoba.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
